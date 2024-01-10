0ca1f09136b113ed07de063f85a24f8161bde4fc
Jonzoe <emilj03@gmail.com>
2024-01-10 18:44:57 +0100

    First commit, code doesn't work

---
+++ Python/Day_1.py
@@ -0,0 +1,27 @@
+f_lines = "1abc2 3"
+ 
+# getting numbers from string
+result = 0
+line = f_lines.split("\n")
+print(f_lines)
+for i in line:
+    first_number = 0
+    second_number = 0
+
+    for x in i:
+        if i.isdigit():
+            first_number = i
+            break
+
+    line = line[::-1]
+
+    for j in line:
+        for x in i:
+            if i.isdigit():
+                second_number_number = j
+                break
+
+result = first_number + second_number
+
+# print result
+print("The numbers list is : " + str(result))
